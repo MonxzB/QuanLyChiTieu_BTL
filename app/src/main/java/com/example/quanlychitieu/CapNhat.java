15223647a634c34036b57a6970d8a334fc8fb788
MonxzB <115096813+MonxzB@users.noreply.github.com>
2023-10-30 07:02:33 +0700

    Add files via upload

--- app/src/main/java/com/example/quanlychitieu/CapNhat.java
+++ app/src/main/java/com/example/quanlychitieu/CapNhat.java
@@ -2,13 +2,29 @@ package com.example.quanlychitieu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class CapNhat extends AppCompatActivity {
+
+    ImageButton imgback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cap_nhat);
+
+        imgback= findViewById(R.id.imgback);
+        Intent intentback= getIntent();
+        Bundle bundle= intentback.getBundleExtra("Cập nhật");
+        imgback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/quanlychitieu/ThuChi.java
+++ app/src/main/java/com/example/quanlychitieu/ThuChi.java
@@ -2,13 +2,39 @@ package com.example.quanlychitieu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class ThuChi extends AppCompatActivity {
+
+    ImageButton imgan, imgmua, imgluong, imgthuong, imgtrangchu, imgthuchi, imgcaidat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thu_chi);
+
+        imgan= findViewById(R.id.imgan);
+        imgmua= findViewById(R.id.imgmua);
+        imgluong= findViewById(R.id.imgluong);
+        imgthuong= findViewById(R.id.imgthuong);
+        imgtrangchu= findViewById(R.id.imgtrangchu);
+        imgthuchi= findViewById(R.id.imgthuchi);
+        imgcaidat= findViewById(R.id.imgcaidat);
+
+        imgan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentan= new Intent(ThuChi.this, CapNhat.class);
+                Bundle bundlean= new Bundle();
+                intentan.putExtra("Cập nhật", bundlean);
+                startActivity(intentan);
+            }
+        });
     }
 }
