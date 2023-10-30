1e48fa26e44677aa183d0f6e1e84f6e66f1c5d49
VNgKhanh04 <vnk4070@gmail.com>
2023-10-31 01:19:06 +0700

    chỉnh sửa

--- app/src/main/java/com/example/quanlychitieu/MainActivity.java
+++ app/src/main/java/com/example/quanlychitieu/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.quanlychitieu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_dang_nhap);
     }
 }
--- app/src/main/java/com/example/quanlychitieu/ThemKhoanChi.java
+++ app/src/main/java/com/example/quanlychitieu/ThemKhoanChi.java
@@ -2,13 +2,16 @@ package com.example.quanlychitieu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ThemKhoanChi extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_khoan_chi);
     }
+
 }
